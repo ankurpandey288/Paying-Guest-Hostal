d972046b39d03fc4342a6679635b6af0f1114a87
Ankur kumar pandey <77723412+ankurpandey288@users.noreply.github.com>
2022-01-20 00:33:52 +0530

    initial commit

---
+++ Parento_Homz/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Parento_Homz
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Parento_Homz/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Parento_Homz.MvcApplication" Language="C#" %>
